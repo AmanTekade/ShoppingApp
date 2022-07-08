960dd5cbd92d7961170a3fe559aac225538652ca
AkshayMangulkar <108930233+AkshayMangulkar@users.noreply.github.com>
2022-07-08 15:39:18 +0530

    Program File

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Deligate
+{
+    class basic
+    {
+        public delegate void del1(); // declaring delegate
+        public static void print()
+        {
+            Console.WriteLine("we are in delegate...");
+        }
+        static void Main(string[] args)
+        {
+            del1 d1 = new del1(print); // making instance of delegate
+            d1.Invoke(); //invoking the method
+            Console.ReadLine();
+        }
+    }
+}
